f65f91c1d6da1de9de41d3a620cb648bc84fdb34
szymeq17 <rysz.szymon@gmail.com>
2022-01-23 22:23:45 +0100

    Move uppercasing to config lib

--- CryptoMonitor/lib/config/config.ml
+++ CryptoMonitor/lib/config/config.ml
@@ -4,7 +4,7 @@ let get_config_json path = Yojson.Basic.from_file path
 
 let json_to_coin json =
   let open Yojson.Basic.Util in
-  let symbol = json |> member "symbol" |> to_string in 
+  let symbol = json |> member "symbol" |> to_string |> String.uppercase_ascii in 
   let price = json |> member "price" |> to_float in
   let currency = json |> member "currency" |> to_string in
   Coin(symbol, price, currency)
