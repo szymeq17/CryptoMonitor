5f790e573e727f82c677bdeb864590a68d13f83b
szymeq17 <rysz.szymon@gmail.com>
2022-01-29 22:16:51 +0100

    Use one request per coin list

--- CryptoMonitor/lib/monitor/monitor.ml
+++ CryptoMonitor/lib/monitor/monitor.ml
@@ -5,8 +5,11 @@ open Notifier
 open Config
 open Coin
 
-let body symbol currency =
-  Client.get (Uri.of_string ("https://min-api.cryptocompare.com/data/price?fsym=" ^ symbol ^ "&tsyms=" ^ currency))
+module Symbols = Set.Make(String)
+module Currencies = Set.Make(String)
+
+let body symbols currencies =
+  Client.get (Uri.of_string ("https://min-api.cryptocompare.com/data/pricemulti?fsyms=" ^ symbols ^ "&tsyms=" ^ currencies))
   >>= fun (_, body) ->
   body |> Cohttp_lwt.Body.to_string 
   >|= fun body -> body
@@ -32,38 +35,58 @@ let perform_action coin current_price =
             | Profit -> price_and_profit_action symbol current_price target_price pocket target_profit currency
             | PriceAndProfit -> price_and_profit_action symbol current_price target_price pocket target_profit currency
   
-let check_prices json_coins = 
-  List.iter (List.map json_coins ~f:json_to_coin) ~f:(fun coin ->
-    match coin with
-    | Inapplicable -> ()
-    | Coin(symbol, _, currency, _, _, _, _) ->
-  let body = Lwt_main.run (body symbol currency) in
+let check_prices coins symbols currencies = 
+  let body = Lwt_main.run (body symbols currencies) in
   let json = Yojson.Basic.from_string body in
   let open Yojson.Basic.Util in
-  try (
-    let price = json |> member currency |> to_float in
-    perform_action coin price) with
-    _ -> 
-      try (
-        let price = json |> member currency |> to_int |> Int.to_float in
-        perform_action coin price) with 
-      _ -> coin_price_not_found symbol)
+  List.iter coins ~f:(fun coin ->
+    match coin with
+     | Inapplicable -> ()
+     | Coin(symbol, _, currency, _, _, _, _) ->
+    try (
+      let price = json |> member symbol |> member currency |> to_float in
+      perform_action coin price) with
+      _ -> 
+        try (
+          let price = json |> member symbol |> member currency |> to_int |> Int.to_float in
+          perform_action coin price) with 
+        _ -> coin_price_not_found symbol)
 
-let rec run_monitor coins interval =
-  check_prices coins;
+let set_to_string set =
+  Set.fold ~f:(fun elem init -> init ^ "," ^ elem) set ~init:""
+
+let get_symbols coins = 
+  let rec find_symbols coins symbols =
+    match coins with
+      | [] -> symbols
+      | Inapplicable :: coins -> find_symbols coins symbols
+      | Coin(symbol, _, _, _, _, _, _) :: coins -> find_symbols coins (Symbols.add symbols symbol)
+  in
+  find_symbols coins Symbols.empty
+
+let get_currencies coins = 
+  let rec find_currencies coins currencies =
+    match coins with
+      | [] -> currencies
+      | Inapplicable :: coins -> find_currencies coins currencies
+      | Coin(_, _, currency, _, _, _, _) :: coins -> find_currencies coins (Currencies.add currencies currency)
+  in
+  find_currencies coins Currencies.empty
+let rec run_monitor coins symbols currencies interval =
+  check_prices coins symbols currencies;
   print_endline "";
   Unix.sleep interval;
-  run_monitor coins interval
+  run_monitor coins symbols currencies interval
 
 let run () =
   let config = get_config_json "config.json" in
   let json_coins = get_coin_list config in
+  let coins = List.map json_coins ~f:json_to_coin in
+  let symbols = set_to_string (get_symbols coins) in
+  let currencies = set_to_string (get_currencies coins) in
   let interval = 
     match get_interval config with
       | Some v -> v
       | None   -> 30
   in
-  run_monitor json_coins interval
-  
-
-
+  run_monitor coins symbols currencies interval
